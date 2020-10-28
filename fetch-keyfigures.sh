aec79d4f3cbef40e20ca3230e8024d8143402341
ofa <ofa@github>
2020-10-29 00:52:55 +0100

    add fetch-keyfigures.sh

---
+++ fetch-keyfigures.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DATA_URL=https://app.stopcovid.gouv.fr/infos/key-figures.json
+DATA_CSV=data.csv
+
+HEADER="date, registered, notified, declared, new_cases"
+FIELDS=".registered, .notification, .qrcodes, .nombrecas"
+
+DATE=$(date +'%F %H:%M')
+
+[ -f $DATA_CSV ] || cat > $DATA_CSV <<< "$HEADER"
+
+data=$(wget -q -O - $DATA_URL \
+| sed 's:labelKey:Key:g; s:valueGlobal:Value:g; s:keyfigure.::g' \
+| jq ". | from_entries | [ $FIELDS ] | @csv" \
+| tr -d \"
+)
+
+sed -i "1 a\\
+$DATE,$data" $DATA_CSV
+
+sed -n 2p $DATA_CSV | tr , "\t"
